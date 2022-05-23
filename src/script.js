355c816f020f5a90c25619d205999463917ddbcc
Tanaz2202 <tanaz.kh.ne@gmail.com>
2022-05-23 23:04:37 +0100

    Future forecast HTML and Javascript added

--- src/script.js
+++ src/script.js
@@ -22,6 +22,51 @@ function formattedDate(timestamp) {
   return `${day} ${hour}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#future-forecast");
+
+  forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+            <div class="future-forecast-date">${formatDay(forecastDay.dt)}</div>
+            <img
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt="futureTemperature"
+              class="future-forecast-icon"
+              width="50px"
+            />
+        <div class="weather-forecast-temperature">
+   <span class="max-temperature">${Math.round(forecastDay.temp.max)}°</span>
+            <span class="min-temperature">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
+            </div>
+          </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(cordinates) {
+  let apiKey = "6ce7450f789843d1a9368eb2e4d194d8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=0${cordinates.lat}&lon=${cordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let cityElement = document.querySelector("#city");
@@ -43,7 +88,6 @@ function displayWeather(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   dateElement.innerHTML = formattedDate(response.data.dt * 1000);
-  console.log(response);
 }
 function searchCity(city) {
   let apiKey = "6ce7450f789843d1a9368eb2e4d194d8";
